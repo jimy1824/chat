9ce63bdcfc018b1cbf762e5e1a38e95988621715
jamshaid <jamshaid1824@gmail.com>
2019-11-07 22:40:52 +0500

    Initial commit

--- chat/models.py
+++ chat/models.py
@@ -15,3 +15,4 @@ class Chat(BaseModel):
     sender=models.ForeignKey(User,related_name='message_sender' ,on_delete=models.CASCADE)
     receiver=models.ForeignKey(User,related_name='message_receiver', on_delete=models.CASCADE)
     message=models.TextField()
+    is_read = models.BooleanField(default=False)
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.views import View
 from account.models import User
+from chat.models import Chat
+
+
+class ChatHome(View):
+    template_name='chat.html'
+
+    def get(self,request,*args,**kwargs):
+        all_users=User.objects.filter(is_superuser=False)
+        messages_list=Chat.objects.filter(is_read=False)
+        return render(request,self.template_name,context={'users':all_users,'messages_list':messages_list})
 
 
 class ChatInbox(View):
     template_name='chat_box.html'
 
-    def get(self,request,*args,**kwargs):
-        return render(request,self.template_name,context={'users':User.objects.all()})
+    def get(self,request,user_id,*args,**kwargs):
+        print(user_id,'user_id')
+        return render(request,self.template_name,context={'room_name':'room_name'})
