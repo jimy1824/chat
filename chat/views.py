3409bc0066dbe262dcf63339d6cef30f25aff6ba
jamshaid <jamshaid1824@gmail.com>
2019-11-08 10:00:12 +0500

    channels

--- chat/views.py
+++ chat/views.py
@@ -8,7 +8,7 @@ class ChatHome(View):
     template_name='chat.html'
 
     def get(self,request,*args,**kwargs):
-        all_users=User.objects.filter(is_superuser=False)
+        all_users=User.objects.filter(is_superuser=False).exclude(id=request.user.id)
         messages_list=Chat.objects.filter(is_read=False)
         return render(request,self.template_name,context={'users':all_users,'messages_list':messages_list})
 
@@ -16,6 +16,6 @@ class ChatHome(View):
 class ChatInbox(View):
     template_name='chat_box.html'
 
-    def get(self,request,user_id,*args,**kwargs):
-        print(user_id,'user_id')
-        return render(request,self.template_name,context={'room_name':'room_name'})
+    def get(self,request,selected_user_id,*args,**kwargs):
+        room_name=str(selected_user_id)+'_'+str(request.user.id)
+        return render(request,self.template_name,context={'room_name':room_name})
